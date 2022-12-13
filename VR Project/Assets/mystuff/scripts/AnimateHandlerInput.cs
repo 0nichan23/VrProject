9dd83e809190db8b9071a73466639e10a4e2e078
RonTheDragon <gweta.ron@gmail.com>
2022-12-13 14:58:04 +0200

    hands supposed to work

---
+++ VR Project/Assets/mystuff/scripts/AnimateHandlerInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandlerInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction; //activate value
+    public Animator handAnimator;
+    public InputActionProperty gripAnimationAction; //select value
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", triggerValue);
+    }
+}
